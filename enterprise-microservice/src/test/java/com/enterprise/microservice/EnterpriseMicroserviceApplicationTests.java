4e7de0029995ab91a568e753798ea374d53dcba4
ssr <jpr_surendra@yahoo.co.in>
2026-06-09 12:45:25 +0530

    create new project

---
+++ enterprise-microservice/src/test/java/com/enterprise/microservice/EnterpriseMicroserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.enterprise.microservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EnterpriseMicroserviceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
